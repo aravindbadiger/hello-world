178e2bb11b6dd1d51c41a8427c11d50b4dea0a58
aravindbadiger <jagfasda@googlemail.com>
2016-07-16 12:24:01 +0530

    HelloWorld Java application

---
+++ src/HelloWorldMain.java
@@ -0,0 +1,29 @@
+import com.ab.trainings.helloworld.classes.SayHelloWorldImpl;
+
+/**
+ * 
+ */
+
+/**
+ * @author Badiger
+ *
+ */
+public class HelloWorldMain {
+
+	/**
+	 * 
+	 */
+	public HelloWorldMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SayHelloWorldImpl shw = new SayHelloWorldImpl();
+		System.out.println(shw.SayHelloWorld());
+	}
+
+}
